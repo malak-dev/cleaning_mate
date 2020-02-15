b36e4a04efca0c41e71203daf21fe3dd7d6f3a8f
alexfalco <afalconer02@gmail.com>
2020-02-15 17:00:27 -0500

    connect to db enabled

--- back_end/db/seeds/clients.sql
+++ back_end/db/seeds/clients.sql
@@ -2,3 +2,7 @@ INSERT INTO clients
   (first_name,last_name,phone_number,email,password,address)
 VALUES
   ('malak', 'younso', '88888888', 'malak-y@live.com', '123445', '1022 saint-mathieu');
+
+
+
+
