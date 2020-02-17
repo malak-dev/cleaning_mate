d229698d03305087a08e67a14787fcc5baaeaf4d
malak-dev <malak-y@live.com>
2020-02-17 15:51:49 -0500

    add main page

--- client/src/components/login.js
+++ client/src/components/login.js
@@ -8,31 +8,31 @@ export default function Login(props) {
     <div className="form">
       <form autoComplete="off" onSubmit={event => event.preventDefault()}>
         <h1 className="form_elements"> Login here </h1>
-        <div className="form_elements">
+        <div className="form_elements" class="edit">
           <div class="form-group">
-            <label >Email address
-          <input
-                class="form-control"
-                aria-describedby="emailHelp"
-                id="exampleInputEmail1"
-                name="email"
-                type="email"
-                placeholder="Enter your email"
-              // value={email}
-              // onChange={(event) => { setEmail(event.target.value) }}
-              /></label>
+            <label >Email address</label>
+            <input
+              class="form-control"
+              aria-describedby="emailHelp"
+              id="exampleInputEmail1"
+              name="email"
+              type="email"
+              placeholder="Enter your email"
+            // value={email}
+            // onChange={(event) => { setEmail(event.target.value) }}
+            />
           </div>
           <div class="form-group">
-            <label >Password
-          <input
-                class="form-control"
-                id="exampleInputPassword1"
-                name="password"
-                type="email"
-                placeholder="Enter your password"
-              // value={password}
-              // onChange={(event) => { setPassword(event.target.value) }}
-              /></label>
+            <label >Password</label>
+            <input
+              class="form-control"
+              id="exampleInputPassword1"
+              name="password"
+              type="email"
+              placeholder="Enter your password"
+            // value={password}
+            // onChange={(event) => { setPassword(event.target.value) }}
+            />
           </div>
 
           <button type="submit" class="btn btn-primary">Login</button>
