58353ba903ba7115429db6bab075300bbc259a7d
malak-dev <malak-y@live.com>
2020-02-24 17:16:05 -0500

    add some appointments

--- back_end/db/seeds/appointments.sql
+++ back_end/db/seeds/appointments.sql
@@ -9,12 +9,12 @@ VALUES
 INSERT INTO appointments
   (date,start_time,hours,booked,cost_per_hour,comment,rating,client_id,provider_id)
 VALUES
-  ('2020-02-02', 9, 1, true, 15, 'Good Work', 5, 2, 1);
+  ('2020-02-02', 9, 1, true, 20, 'Good Work', 5, 2, 1);
 
 INSERT INTO appointments
   (date,start_time,hours,booked,cost_per_hour,comment,rating,client_id,provider_id)
 VALUES
-  ('2020-02-02', 10, 1, true, 15, 'Not the best', 4, 2, 1);
+  ('2020-02-02', 10, 1, true, 30, 'Not the best', 4, 2, 1);
 
 
 
@@ -23,7 +23,7 @@ VALUES
 INSERT INTO appointments
   (date,start_time,hours,booked,cost_per_hour,comment,rating,client_id,provider_id)
 VALUES
-  ('2020-03-02', 10, 1, true, 15, null, null, 1, 1);
+  ('2020-03-02', 10, 1, true, 20, null, null, 1, 1);
 
 INSERT INTO appointments
   (date,start_time,hours,booked,cost_per_hour,comment,rating,client_id,provider_id)
@@ -34,18 +34,18 @@ VALUES
 INSERT INTO appointments
   (date,start_time,hours,booked,cost_per_hour,comment,rating,client_id,provider_id)
 VALUES
-  ('2020-03-02', 12, 1, true, 15, null, null, 2, 8);
+  ('2020-04-02', 12, 1, true, 20, null, null, 2, 8);
 
 
 INSERT INTO appointments
   (date,start_time,hours,booked,cost_per_hour,comment,rating,client_id,provider_id)
 VALUES
-  ('2020-03-02', 13, 1, true, 15, null, null, 1, 7);
+  ('2020-05-02', 13, 1, true, 23, null, null, 1, 7);
 
 INSERT INTO appointments
   (date,start_time,hours,booked,cost_per_hour,comment,rating,client_id,provider_id)
 VALUES
-  ('2020-03-02', 14, 1, true, 15, null, null, 2, 1);
+  ('2020-05-02', 14, 1, true, 20, null, null, 2, 1);
 
 
 
@@ -54,67 +54,48 @@ VALUES
 INSERT INTO appointments
   (date,start_time,hours,booked,cost_per_hour,comment,rating,client_id,provider_id)
 VALUES
-  ('2020-03-02', 9, 1, false, 15, null, null, null, 6);
+  ('2020-04-01', 9, 1, false, 20, null, null, null, 6);
 
 INSERT INTO appointments
   (date,start_time,hours,booked,cost_per_hour,comment,rating,client_id,provider_id)
 VALUES
-  ('2020-03-02', 8, 1, false, 15, null, null, null, 2);
-
-
-
-INSERT INTO appointments
-  (date,start_time,hours,booked,cost_per_hour,comment,rating,client_id,provider_id)
-VALUES
-  ('2020-03-02', 8, 1, false, 15, null, null, null, 3);
-
-
-INSERT INTO appointments
-  (date,start_time,hours,booked,cost_per_hour,comment,rating,client_id,provider_id)
-VALUES
-  ('2020-03-02', 9, 1, false, 15, null, null, null, 1);
+  ('2020-03-03', 8, 1, false, 15, null, null, null, 2);
 
 
 
 INSERT INTO appointments
   (date,start_time,hours,booked,cost_per_hour,comment,rating,client_id,provider_id)
 VALUES
-  ('2020-03-03', 9, 1, false, 15, null, null, null, 2);
+  ('2020-03-03', 8, 1, false, 20, null, null, null, 3);
+
 
 INSERT INTO appointments
   (date,start_time,hours,booked,cost_per_hour,comment,rating,client_id,provider_id)
 VALUES
-  ('2020-03-03', 10, 1, false, 15, null, null, null, 3);
-
-INSERT INTO appointments
-  (date,start_time,hours,booked,cost_per_hour,comment,rating,client_id,provider_id)
-VALUES
-  ('2020-03-03', 11, 1, false, 15, null, null, null, 4);
-
-INSERT INTO appointments
-  (date,start_time,hours,booked,cost_per_hour,comment,rating,client_id,provider_id)
-VALUES
-  ('2020-03-03', 12, 1, false, 15, null, null, null, 5);
-
-
-
+  ('2020-03-03', 9, 1, false, 23, null, null, null, 1);
 
 
 
 INSERT INTO appointments
   (date,start_time,hours,booked,cost_per_hour,comment,rating,client_id,provider_id)
 VALUES
-  ('2020-03-03', 10, 1, false, 15, null, null, null, 2);
+  ('2020-03-06', 9, 1, false, 20, null, null, null, 2);
 
 INSERT INTO appointments
   (date,start_time,hours,booked,cost_per_hour,comment,rating,client_id,provider_id)
 VALUES
-  ('2020-03-03', 11, 1, false, 15, null, null, null, 2);
+  ('2020-04-02', 10, 1, false, 15, null, null, null, 3);
 
 INSERT INTO appointments
   (date,start_time,hours,booked,cost_per_hour,comment,rating,client_id,provider_id)
 VALUES
-  ('2020-03-03', 12, 1, false, 15, null, null, null, 2);
+  ('2020-05-01', 11, 1, false, 20, null, null, null, 4);
+
+INSERT INTO appointments
+  (date,start_time,hours,booked,cost_per_hour,comment,rating,client_id,provider_id)
+VALUES
+  ('2020-05-01', 12, 1, false, 20, null, null, null, 5);
+
 
 
 
@@ -123,9 +104,38 @@ VALUES
 INSERT INTO appointments
   (date,start_time,hours,booked,cost_per_hour,comment,rating,client_id,provider_id)
 VALUES
-  ('2020-02-03', 11, 1, false, 15, null, null, 1, 2);
+  ('2020-03-02', 10, 1, false, 20, null, null, null, 2);
 
 INSERT INTO appointments
   (date,start_time,hours,booked,cost_per_hour,comment,rating,client_id,provider_id)
 VALUES
-  ('2020-02-03', 12, 1, false, 15, null, null, 1, 2);
+  ('2020-02-03', 11, 1, true, 15, null, null, 1, 2);
+
+INSERT INTO appointments
+  (date,start_time,hours,booked,cost_per_hour,comment,rating,client_id,provider_id)
+VALUES
+  ('2020-02-05', 12, 1, true, 15, null, null, 2, 2);
+
+
+
+
+
+INSERT INTO appointments
+  (date,start_time,hours,booked,cost_per_hour,comment,rating,client_id,provider_id)
+VALUES
+  ('2020-02-03', 11, 1, false, 15, null, null, 1, 5);
+
+INSERT INTO appointments
+  (date,start_time,hours,booked,cost_per_hour,comment,rating,client_id,provider_id)
+VALUES
+  ('2020-03-03', 12, 1, false, 15, null, null, 2, 2);
+
+INSERT INTO appointments
+  (date,start_time,hours,booked,cost_per_hour,comment,rating,client_id,provider_id)
+VALUES
+  ('2020-03-03', 11, 1, false, 25, null, null, 1, 3);
+
+INSERT INTO appointments
+  (date,start_time,hours,booked,cost_per_hour,comment,rating,client_id,provider_id)
+VALUES
+  ('2020-03-03', 12, 1, false, 20, null, null, 2, 4);
--- back_end/db/seeds/providers.sql
+++ back_end/db/seeds/providers.sql
@@ -3,50 +3,50 @@
 INSERT INTO providers
   (first_name,last_name,phone_number,email,password,address,lat,lon)
 VALUES
-  ('alex1', 'falco1', 5148350149, 'a@gmail.com', '12345', '221 St-Jacques Montreal', 45.541276, -73.610938);
+  ('Alex', 'falco1', 5148350149, 'a@gmail.com', '12345', '221 St-Jacques Montreal', 45.541276, -73.610938);
 
 INSERT INTO providers
   (first_name,last_name,phone_number,email,password,address,lat,lon)
 VALUES
-  ('Mr. B', 'falco2', 5148350149, 'b@gmail.com', '12345', '221 St-Jacques Montreal', 45.526129, -73.630902);
+  ('Aron', 'falco2', 5148350149, 'b@gmail.com', '12345', '221 St-Jacques Montreal', 45.526129, -73.630902);
 
 INSERT INTO providers
   (first_name,last_name,phone_number,email,password,address,lat,lon)
 VALUES
-  ('alex3', 'falco3', 5148350149, 'c@gmail.com', '12345', '221 St-Jacques Montreal', 45.549526, -73.618463);
+  ('Gourge', 'falco3', 5148350149, 'c@gmail.com', '12345', '221 St-Jacques Montreal', 45.549526, -73.618463);
 
 INSERT INTO providers
   (first_name,last_name,phone_number,email,password,address,lat,lon)
 VALUES
-  ('Mr. D', 'falco4', 5148350149, 'd@gmail.com', '12345', '221 St-Jacques Montreal', 45.530880 , -73.621976);
+  ('Maria', 'falco4', 5148350149, 'd@gmail.com', '12345', '221 St-Jacques Montreal', 45.530880 , -73.621976);
 
 INSERT INTO providers
   (first_name,last_name,phone_number,email,password,address,lat,lon)
 VALUES
-  ('alex5', 'falco5', 5148350149, 'e@gmail.com', '12345', '221 St-Jacques Montreal', 45.554603, -73.635642);
+  ('Lana', 'falco5', 5148350149, 'e@gmail.com', '12345', '221 St-Jacques Montreal', 45.554603, -73.635642);
 
 INSERT INTO providers
   (first_name,last_name,phone_number,email,password,address,lat,lon)
 VALUES
-  ('Mr. F', 'falco6', 5148350149, 'f@gmail.com', '12345', '221 St-Jacques Montreal', 45.530880, -73.621976);
+  ('Malak', 'falco6', 5148350149, 'f@gmail.com', '12345', '221 St-Jacques Montreal', 45.530880, -73.621976);
 
 INSERT INTO providers
   (first_name,last_name,phone_number,email,password,address,lat,lon)
 VALUES
-  ('alex7', 'falco7', 5148350149, 'g@gmail.com', '12345', '221 St-Jacques Montreal', 45.558210, -73.645065);
+  ('Sara', 'falco7', 5148350149, 'g@gmail.com', '12345', '221 St-Jacques Montreal', 45.558210, -73.645065);
 
 INSERT INTO providers
   (first_name,last_name,phone_number,email,password,address,lat,lon)
 VALUES
-  ('Mr. H', 'falco8', 5148350149, 'h@gmail.com', '12345', '221 St-Jacques Montreal', 45.528958, -73.606510);
+  ('Noura', 'falco8', 5148350149, 'h@gmail.com', '12345', '221 St-Jacques Montreal', 45.528958, -73.606510);
 
 INSERT INTO providers
   (first_name,last_name,phone_number,email,password,address,lat,lon)
 VALUES
-  ('alex9', 'falco9', 5148350149, 'i@gmail.com', '12345', '1022 saint mathieu', 45.551410, -73.631420);
+  ('Joly', 'falco9', 5148350149, 'i@gmail.com', '12345', '1022 saint mathieu', 45.551410, -73.631420);
 
 INSERT INTO providers
   (first_name,last_name,phone_number,email,password,address,lat,lon)
 VALUES
-  ('Mr. J', 'falco10', 5148350149, 'j@gmail.com', '12345', '221 St-Jacques Montreal', 45.535066, -73.595317);
+  ('Lon', 'falco10', 5148350149, 'j@gmail.com', '12345', '221 St-Jacques Montreal', 45.535066, -73.595317);
 
