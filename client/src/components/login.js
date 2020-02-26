b4271040a0e8e8f496a08f7c260eeb79136342a9
malak-dev <malak-y@live.com>
2020-02-26 11:41:33 -0500

    add style

--- client/src/components/login.js
+++ client/src/components/login.js
@@ -21,7 +21,8 @@ export default function Login(props) {
           <br />
         </section>
         <h> Login In Our App</h>
-
+        <br />
+        <br />
         <div className="form_elements" >
           <div class="form-group">
             <input
